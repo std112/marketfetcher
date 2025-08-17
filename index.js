23cc3faee6aee7414e91de1a34c3f37dc96fbd6a
std112 <chingkeepaymaya2022@gmail.com>
2025-08-17 09:51:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const STEAM_API_KEY = '';
+const STEAM_API_KEY = 'D7EA583D69A9ACAAB90D1ACAB054C397';
 
 app.use(cors());
 app.use(express.json());
@@ -46,4 +46,5 @@ app.post('/api/steamProfile', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+
 
